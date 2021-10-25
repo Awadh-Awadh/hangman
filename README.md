768610ee26743d972eb0689cb00737a33fb318ef
Awadh-Awadh <awadh.awadhawadh.awadh@student.moringaschool.com>
2021-10-25 23:47:27 +0300

    separe logic from the visual and add logic to reduce lives when the user fails

---
+++ README.md
@@ -0,0 +1,22 @@
+## Hangman
+
+
+- Hangman is a guessing game for players. The player thinks of a word, phrase or sentence and tries to guess it by suggesting letters within a certain number of guesses. If the player guessing chances comes to an end he/she loses the game.
+
+## How its Played
+
+- A player guesses a letter
+- Gets a response if the letter is contained in the word else looses a life
+- If players fails to make a correct guess and all the lives die the player looses the game 
+- when a player makes correct letter guesses of the given word he wins
+
+
+## To play
+
+- clone the repo in your local machine <git clone reponame>
+- run the file and play in the command line <python filename>
+
+
+
+
+# made with love Awadh
--- hangman.py
+++ hangman.py
@@ -1,66 +1,17 @@
 import random
+from hangword import *
+from hangart import *
+
+print(logo)
 
 
-stages = ['''
-  +---+
-  |   |
-  O   |
- /|\  |
- / \  |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
- /|\  |
- /    |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
- /|\  |
-      |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
- /|   |
-      |
-      |
-=========''', '''
-  +---+
-  |   |
-  O   |
-  |   |
-      |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
-      |
-      |
-      |
-=========
-''', '''
-  +---+
-  |   |
-      |
-      |
-      |
-      |
-=========
-''']
+
 
 world_list = ['mouse', 'baboon', 'python', 'code']
+
+# Gets a random word and creates a list out of the letters
+
 choosen_word = list(random.choice(world_list))
-print(choosen_word)
 
 #  Tricks
 # res = []
@@ -73,26 +24,33 @@ print(choosen_word)
 
 
 lives = 6
-
 end_game = False
 
-while not end_game:
-    res = []
-    for _ in range(len(choosen_word)):
-      res += '_'
-    print(res)
-    val = input("Guess letter \n") 
+# creating a list of randomly choosen word replaced with dashes
+
+res = []
+for _ in range(len(choosen_word)):
+  res += '_'
+
+#Iteration as long as the game has not ended
+while not end_game:    
+    val = input("Guess letter \n")    
+    if val in res:
+      print(f"{val} already exist Please try another letter")
     for position in range(len(choosen_word)):
        letter = choosen_word[position]
        if val == letter :
         res[position] = letter
+    print(" ".join(res))
     if val not in choosen_word:
+        print(f"You have chosen {val} that's not in the word. You loose a life")
         lives -=1
         if lives == 0:
             print("You lost")
             end_game = True
-
-    if "_" in res:
+    # The conditions checks that all the letters guesed match with the letters in the word and ends the game
+    if "_" not in res:
         print("You won")
         end_game = True
+    print(stages[lives])
         
